d7817c1ee09d3e7562f0490e83708a02a3b44631
ko-he- <koheiyatsushiro@yahoo.co.jp>
2016-09-20 09:41:46 +0900

    Refactoring

--- features/emmerich_to_sslvpn_nc.feature
+++ features/emmerich_to_sslvpn_nc.feature
@@ -4,10 +4,8 @@ Feature: Emmerich Inc 社からSSLVPNの実行
   Emmerich Inc 社からSSLVPNをさせたい
   なぜならEmmerich Inc 社の開発者はSSLVPNで開発環境にアクセスするから
 
-  Scenario: Fahey Inc 社からSSLVPNサーバにアクセス
+  Scenario: Emmerich Inc 社からSSLVPNサーバにアクセス
     Given Emmerich Inc 社内部のクライアント
     And SSLVPNサーバ
-    When SSLVPNサーバにEmmerich Inc 社内部のクライアンからnc(80)で疎通確認
-    And SSLVPNサーバにEmmerich Inc 社内部のクライアンからnc(443)で疎通確認
-    Then nc(80)で疎通成功
-    And nc(443)で疎通成功
+    When Emmerich Inc 社内部のクライアンからFahey Inc 社にSSLVPN実行
+    Then SSLVPN成功
--- features/step_definitions/nc_steps.rb
+++ features/step_definitions/nc_steps.rb
@@ -1,12 +1,15 @@
-When(/^SSLVPNサーバにEmmerich Inc 社内部のクライアンからnc\((\d+)\)で疎通確認$/) do |port_num|
-  run "sudo ip netns exec #{@vpn_server.name} bash -c 'echo OK | nc -l #{port_num} &'"
+When(/^Emmerich Inc 社内部のクライアンからFahey Inc 社にSSLVPN実行$/) do
+  run "sudo ip netns exec #{@vpn_server.name} bash -c 'echo OK | nc -l 80 &'"
+  run "sudo ip netns exec #{@vpn_server.name} bash -c 'echo OK | nc -l 443 &'"
 
   cd('.') do
-    @emmerich_pc.exec "nc 203.0.113.5 #{port_num} > log/nc_#{port_num}.log"
+    @emmerich_pc.exec "nc 203.0.113.5 80 > log/nc_80.log"
+    @emmerich_pc.exec "nc 203.0.113.5 443 > log/nc_443.log"
   end
 end
 
-Then(/^nc\((\d+)\)で疎通成功$/) do |port_num|
-  step %(the file "log/nc_#{port_num}.log" should contain "OK")
+Then(/^SSLVPN成功$/) do
+  step %(the file "log/nc_80.log" should contain "OK")
+  step %(the file "log/nc_443.log" should contain "OK")
 end
 
