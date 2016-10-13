d9949ac235d579a9383413fecdd121eaa3d53edb
ko-he- <koheiyatsushiro@yahoo.co.jp>
2016-10-13 16:20:50 +0900

    Add query_dmz_dns_from_dmz.feature

---
+++ features/step_definitions/dns_query_steps.rb
@@ -0,0 +1,14 @@
+When(/^ヨーヨーダイン社のDMZからヨーヨーダイン社のDMZ内のDNSサーバへの問い合わせ$/) do
+  Thread.start { @dns_server.exec "bash -c 'echo OK | nc -l 53'" }
+  Thread.start { @dns_server.exec "bash -c 'echo OK | nc -lu 53'" }
+
+  cd('.') do
+    @dmz_server.exec "nc 10.10.0.10 53 > log/nc_53.log"
+    @dmz_server.exec "nc -uv -w4 10.10.0.10 53 > log/nc_udp_53.log"
+  end
+end
+
+Then(/^問い合わせ成功$/) do
+  step %(the file "log/nc_53.log" should contain "OK")
+  step %(the file "log/nc_udp_53.log" should contain "OK")
+end
