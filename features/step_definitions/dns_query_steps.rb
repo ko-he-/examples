c212fafe6da2e6544b14d38141bf4433f172fd67
ko-he- <koheiyatsushiro@yahoo.co.jp>
2016-10-14 10:54:02 +0900

    Add sleep in query step

--- features/step_definitions/dns_query_steps.rb
+++ features/step_definitions/dns_query_steps.rb
@@ -1,6 +1,7 @@
 When(/^ヨーヨーダイン社のDMZからヨーヨーダイン社のDMZ内のDNSサーバへの問い合わせ$/) do
   Thread.start { @dns_server.exec "bash -c 'echo OK | nc -l 53'" }
   Thread.start { @dns_server.exec "bash -c 'echo OK | nc -lu 53'" }
+  sleep(3)
 
   cd('.') do
     @dmz_server.exec "nc 10.10.0.10 53 > log/nc_53.log"
